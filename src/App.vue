2c357b69b237976a17f9ee4df1021bdc93fa4afa
huangjiahan66 <1198434221@qq.com>
2023-10-02 15:05:59 +0800

    竖形柱状图

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@
     </div>
     <div class="flex-1 bg-opacity-50 bg-slate-800 p-3 flex flex-col">
       <!-- 竖向柱状图 -->
-      <VerticalBar class="h-1/3 box-border pb-4" />
+      <VerticalBar class="h-1/3 box-border pb-4" :data="data.serverData" />
       <!-- 环形资源站比图 -->
       <RadiueBar class="h-1/3 box-border pb-4" />
       <!-- 文档云图 -->
--- src/components/VerticalBar.vue
+++ src/components/VerticalBar.vue
@@ -1,7 +1,90 @@
 <template>
-  <div>竖向柱状图</div>
+  <div>
+    <div>服务资源占用比</div>
+    <div ref="target" class="w-full h-full"></div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted, ref, watch } from "vue";
+import * as echarts from "echarts";
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true,
+  },
+});
+
+// dom
+const target = ref(null);
+let mChart = null;
+onMounted(() => {
+  mChart = echarts.init(target.value);
+  randerChart();
+});
+
+const randerChart = () => {
+  const options = {
+    xAxis: {
+      type: "category",
+      data: props.data.servers.map((item) => item.name),
+      axisLabel: {
+        color: "#9EB1C8",
+      },
+    },
+    // 如果y轴展示数据  就是type value
+    yAxis: {
+      type: "value",
+      show: false,
+      max: function (value) {
+        return parseInt(value.max * 1.2);
+      },
+    },
+    grid: {
+      top: 16,
+      right: 0,
+      bottom: 26,
+      left: -26,
+      containLabel: true,
+    },
+    series: [
+      {
+        // 图表类型
+        type: "bar",
+        // 数据筛选
+        data: props.data.servers.map((item) => ({
+          name: item.name,
+          value: item.value,
+        })),
+
+        // 每个轴的样式
+        itemStyle: {
+          color: "#479AD3", // 设置柱子的颜色
+          barBorderRadius: 5, // 设置柱子的圆角
+          shadowColor: "rgba(0, 0, 0, 0.3)", // 设置柱子的阴影颜色
+          shadowBlur: 5, // 设置柱子的阴影模糊大小
+        },
+        // 轴宽度
+        barWidth: 12,
+        // 轴上的字体
+        label: {
+          show: true,
+          // 设置标签位置为右侧
+          position: "right",
+          textStyle: {
+            // 设置标签文本颜色
+            color: "#fff",
+          },
+          position: "top",
+          formatter: "{c}%",
+        },
+      },
+    ],
+  };
+  mChart.setOption(options);
+};
+
+watch(() => props.data, randerChart);
+</script>
 
 <style lang="scss" scoped></style>
